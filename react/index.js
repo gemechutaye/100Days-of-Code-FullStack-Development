72c86b7b9325274113acedb9b029930ac5880754
Gemechu Taye <Geme.t07@gmail.com>
2022-02-01 20:41:16 +0530

    Organizing components

--- react/index.js
+++ react/index.js
@@ -183,3 +183,61 @@ import ReactDOM from "react-dom"
     ReactDOM.render(<Page />, document.getElementById("root"))
 */
 
+
+// Parents or Child Components
+
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png"/>
+            </nav>
+        </header>
+    )
+}
+
+function NewBody() {
+    return (
+           <div>
+                <h1>Reasons I'm excited to learn React</h1>
+                   <ol>
+                        <li>It's a popular library, so I'll be 
+                        able to fit in with the cool kids!</li>
+                        <li>I'm more likely to get a job as a developer
+                        if I know React</li>
+                    </ol>
+           </div>
+    )
+}
+
+function Footer() {
+    return (
+            <footer>
+              <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <NewBody />
+            <footer/>
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
