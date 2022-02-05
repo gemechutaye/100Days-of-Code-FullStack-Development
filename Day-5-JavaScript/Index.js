e3acb194ea37e8b461278ee5c285bff6ef242e2d
Gemechu Taye <Geme.t07@gmail.com>
2022-02-05 09:57:22 +0530

    Day 6 - building a apassanger counter app

--- Day-5-JavaScript/Index.js
+++ Day-5-JavaScript/Index.js
@@ -1,2 +1,25 @@
+/* first Javascript variable
 let myAge = 23;
 console.log(myAge);
+*/
+
+/* Basic Mathimatical Operations
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
+console.log(count)
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+////// example 
+
+// let myAge = 23;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge );
+*/
+
+
+
