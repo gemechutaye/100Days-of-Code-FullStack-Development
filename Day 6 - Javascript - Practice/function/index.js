88628966fceeafde34a103ab399767c39d4e04fd
Gemechu Taye <Geme.t07@gmail.com>
2022-02-05 10:43:25 +0530

    writing simple function

---
+++ Day 6 - Javascript - Practice/function/index.js
@@ -0,0 +1,30 @@
+/* using function
+// Setting up the the race 🏎 🏎 🏎
+
+function countdown(){
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+}
+countdown()
+countdown()
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+
+*/
+
+/* writing my first simple function output
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+function plus() {
+    console.log(42)
+}
+plus()
+*/
+
