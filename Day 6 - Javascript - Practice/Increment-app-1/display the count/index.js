0ce67628f8e191a8879b5be42ce998c39dfe7b93
Gemechu Taye <Geme.t07@gmail.com>
2022-02-05 13:58:44 +0530

    simple console log experiment

--- Day 6 - Javascript - Practice/Increment-app-1/display the count/index.js
+++ Day 6 - Javascript - Practice/Increment-app-1/display the count/index.js
@@ -14,7 +14,6 @@ count = count - 4
 console.log(count)
 
 
-
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
 // Console.log the value after each step
@@ -53,6 +52,7 @@ function increment() {
 
 // creat a save button 
 
+/*
 let countEl = document.getElementById('count-el')
 let count = 0
 
@@ -64,3 +64,37 @@ function increment() {
 function save() {
     console.log(count)
 }
+*/
+
+/*
+let message = "You have three new notifications"
+console.log(message);
+
+
+
+let username = "per"
+let message = "You have tree new notifications"
+console.log("username + ", " + message + "!" ")
+
+// Create a variable, messageToUser, that contains the message we have logged
+
+let messageToUser = message + ", " + username + "!"
+console.log(messageToUser)
+*/
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+/* log greetings to console
+
+let name = "Gemechu Taye"
+let greetings = "Hey there, my name is "
+
+let myGreetings = greetings + name
+console.log(myGreetings);
+
+*/
+
