a1724349a641e9257909a5285be659cea1bf1ed7
Gemechu Taye <Geme.t07@gmail.com>
2022-02-05 15:18:05 +0530

    First JS app - Builted a passenger counter app

--- Day 6 - Javascript - Practice/complete-passange-cout-app/index.js
+++ Day 6 - Javascript - Practice/complete-passange-cout-app/index.js
@@ -1,31 +1,16 @@
-/* Reassigning and incrementing
-
-let count = 5
-count = 3
-count = 1
-
-// increment 
-
-count = count + 1
-count = count + 5
-count = count + 10
-count = count - 4
-
-console.log(count)
-
-
-
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-
-let bonusPoints = 50;
-bonusPoints = bonusPoints + 50;
-bonusPoints = bonusPoints - 75
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
-*/
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
-    console.log("The button was clicked")
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+// set the count to 0
+    countEl.textContent = 0
+    count = 0
 }
