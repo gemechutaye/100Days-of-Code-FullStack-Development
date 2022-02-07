5d381cd6beca699b07e4c02c48a1950703a4ffc7
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 12:09:54 +0530

    Display the cards

--- Day 7 - Javascript/Blackjack game/index.js
+++ Day 7 - Javascript/Blackjack game/index.js
@@ -80,3 +80,32 @@ function startGame() {
 }
 */
 
+/* Display the cards
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    // 3. Render the cars on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+*/
