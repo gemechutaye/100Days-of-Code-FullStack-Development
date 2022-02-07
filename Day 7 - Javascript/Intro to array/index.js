73e2b221d433b92069113117517f91f5c33251e5
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 15:26:50 +0530

    Arrays with multiple data types

--- Day 7 - Javascript/Intro to array/index.js
+++ Day 7 - Javascript/Intro to array/index.js
@@ -1,4 +1,5 @@
-// Arrays - ordered lists of items
+/* intro to array
+//Arrays - ordered lists of items
 
 let featuredPosts = [
     "Check out my Netflix clone",
@@ -11,4 +12,50 @@ let featuredPosts = [
 
 let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
 
- 
+*/
+
+/* array indexes
+// Arrays - ordered lists of items
+
+// 0 indexed
+let featuredPosts = [
+    "Check out my Netflix clone",        // 0 
+    "Here's the code for my project",    // 1
+    "I've just relaunched my portfolio"  // 2
+]
+
+// console.log( featuredPosts[2] )
+
+let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+
+// Make the following appear in the console:
+// Frontend developer at Xeneta
+// People counter for Norstat
+// CEO at Scrimba
+
+console.log(experience[1])
+console.log(experience[2])
+console.log(experience[0])
+*/
+
+
+/* Arrays with multiple data types
+// Array - ordered list of items - coposite / complex data type
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+
+let experiance = [
+    "Gemechu Taye",
+    "23",
+    "true"
+]
+
+console.log( experiance[0] )
+console.log( experiance[1] )
+console.log( experiance[2] )
+
+*/
+
+
--- Day 7 - Javascript/all practice/index.js
+++ Day 7 - Javascript/all practice/index.js
@@ -433,4 +433,62 @@ function newCard() {
 
 */
 
+/* intro to array
+//Arrays - ordered lists of items
 
+let featuredPosts = [
+    "Check out my Netflix clone",
+    "Here's the code for my project",
+    "I've just relaunched my portfolio"
+]
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+
+*/
+
+/* array indexes
+// Arrays - ordered lists of items
+
+// 0 indexed
+let featuredPosts = [
+    "Check out my Netflix clone",        // 0 
+    "Here's the code for my project",    // 1
+    "I've just relaunched my portfolio"  // 2
+]
+
+// console.log( featuredPosts[2] )
+
+let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+
+// Make the following appear in the console:
+// Frontend developer at Xeneta
+// People counter for Norstat
+// CEO at Scrimba
+
+console.log(experience[1])
+console.log(experience[2])
+console.log(experience[0])
+*/
+
+
+/* Arrays with multiple data types
+// Array - ordered list of items - coposite / complex data type
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+
+let experiance = [
+    "Gemechu Taye",
+    "23",
+    "true"
+]
+
+console.log( experiance[0] )
+console.log( experiance[1] )
+console.log( experiance[2] )
+
+*/
