c72904048bb92f7237577666c2a67b5ba23aa9ba
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 16:20:45 +0530

    Write my first for loop

---
+++ Day 7 - Javascript/for loop/index.js
@@ -0,0 +1,30 @@
+/* counting in Javascript
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START           FINISH       STEP SIZE
+for ( let count = 10;  count < 21;  count += 1 )  {
+    
+    console.log(count)
+
+}
+*/
+
+/* Write my first for loop
+
+// for (let i = 0; i < 6; i += 1) {
+//     console.log(i)
+// }
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+for (let i = 10; i < 101; i += 10) {
+    console.log(i)
+}
+// Use console.log to log out the numbers
+
+*/
