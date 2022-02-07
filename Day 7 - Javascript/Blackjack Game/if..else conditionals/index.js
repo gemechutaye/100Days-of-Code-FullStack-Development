c8f1a8748a3f6e9e72e2c2c71e9131ee0eb42b89
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 10:21:25 +0530

    if..else conditionals

---
+++ Day 7 - Javascript/Blackjack Game/if..else conditionals/index.js
@@ -0,0 +1,74 @@
+/* 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard + 6 // 23
+
+// if else conditionals
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
+*/
+
+/*
+// my first if...else statement 
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 21
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if (age <= 20) {
+    console.log("You can not enter the club!")
+} else {
+    console.log("Welcome!")
+}
+*/
+
+/* if/else..if/else statement
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let age = 101
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (age < 100) {
+    console.log("Not elegible")
+} else if (age === 100) {
+    console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not elegible, you have already gotten one")
+}
+*/
+
+/* The if..else statement for the game
+let firstCard = 10
+let secondCard = 10
+let sum = firstCard + secondCard 
+
+// Write the conditional according to these rules:
+
+// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
+// else -> "You're out of the game! 😭"
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
+*/
+
