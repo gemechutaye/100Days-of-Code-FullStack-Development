58ae768ec0c812a85a3811f0112768e709f1ec6b
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 15:44:59 +0530

    Adding and removing items from arrays

--- Day 7 - Javascript/all practice/index.js
+++ Day 7 - Javascript/all practice/index.js
@@ -492,3 +492,47 @@ console.log( experiance[1] )
 console.log( experiance[2] )
 
 */
+
+/* Adding and removing items from array!
+// let cards = [7, 4]
+// cards.push(6)
+//console.log(cards)
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage)
+
+console.log(messages [0])
+console.log(messages [1])
+console.log(messages [2])
+console.log(messages [3])
+ */
+
+/*  Adding and removing items from arrays
+let cards = [7, 4]
+cards.push(6)
+//console.log(cards)
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage)
+console.log(messages)
+
+// How can you remove the last item in an array? Try to google it!
+messages.pop()
+console.log(messages)
+*/
