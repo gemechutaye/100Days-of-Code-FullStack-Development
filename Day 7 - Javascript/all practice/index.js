5ba687a16f6bf6d2e682e08863f1f694465f30fa
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 12:40:06 +0530

    Rename the startGame function

--- Day 7 - Javascript/all practice/index.js
+++ Day 7 - Javascript/all practice/index.js
@@ -349,6 +349,44 @@ function newCard() {
     // 3. Call startGame()
     startGame()
 }
+*/
 
+/* Rename the startGame function
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+// Create a new function called startGame() that calls renderGame()
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = 6
+    sum += card
+    renderGame()
+}
 
 */
