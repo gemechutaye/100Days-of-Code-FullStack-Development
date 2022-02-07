7d31ec332d78b22b77ec5d12f81196eacd5b3be5
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 13:58:00 +0530

    Solving our cards problem with an array

--- Day 7 - Javascript/all practice/index.js
+++ Day 7 - Javascript/all practice/index.js
@@ -390,3 +390,47 @@ function newCard() {
 }
 
 */
+
+/* Solving our cards problem with an array
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard] // array - ordered list of items
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    // render out firstCard and secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    // render out ALL the cards we have
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = 6
+    sum += card
+    renderGame()
+}
+
+*/
+
+
