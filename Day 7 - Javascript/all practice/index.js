3b4dcf6cbb6e0a8a245fef9f75c1c4aa1bffebe0
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 12:33:29 +0530

    Add to the sum when newCard is clicked 3:16

--- Day 7 - Javascript/all practice/index.js
+++ Day 7 - Javascript/all practice/index.js
@@ -306,10 +306,49 @@ function startGame() {
     messageEl.textContent = message
 }
 
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+
 function newCard() {
     console.log("Drawing a new card from the deck!")
 }
+*/
 
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+/* Add to the sum when newCard is clicked
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    // 1. Create a card variable, and hard code its value to a number (2-11)
+    let card = 6
+    // 2. Add the new card to the sum variable
+    sum += card
+    // 3. Call startGame()
+    startGame()
+}
+
 
 */
