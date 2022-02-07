eefcf321d3a61d2c5f443a9a420a31a55ef69a20
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 11:52:23 +0530

    Contributed to display a message on screen

--- Day 7 - Javascript/Blackjack game/index.js
+++ Day 7 - Javascript/Blackjack game/index.js
@@ -1,3 +1,5 @@
+
+/* make a start button
 let firstCard = 10
 let secondCard = 4
 let sum = firstCard + secondCard
@@ -19,4 +21,32 @@ function startGame() {
         isAlive = false
     }
     console.log(message)    
+}
+
+*/
+
+// Display a message
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    // 2. Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message
 }
