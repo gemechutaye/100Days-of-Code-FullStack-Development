dc1151d2dea326b8fe8e24aac3a1befb177ffb3c
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 10:48:49 +0530

    Practice boolean condtions

--- Day 7 - Javascript/Blackjack Game/if..else conditionals/index.js
+++ Day 7 - Javascript/Blackjack Game/if..else conditionals/index.js
@@ -72,3 +72,74 @@ if (sum <= 20) {
 }
 */
 
+/* Add a hasBlackJack variable, and boolean boolean expression
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
+} else {
+    console.log("You're out of the game! 😭")
+}
+
+// CASH OUT!
+console.log(hasBlackJack)
+
+*/
+
+/* Add isAlive Variable, with booleans expression
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard + 4
+let hasBlackJack = false
+let isAlive = true
+// 1. Create a variable called isAlive and assign it to true
+
+// 2. Flip its value to false in the appropriate code block 
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
+} else {
+    console.log("You're out of the game! 😭")
+    isAlive = false
+}
+// 3. Log it out to check that you're doing it right
+console.log(isAlive)
+*/
+
+/* practice boolean condtions
+
+console.log(4 === 3)  // false
+console.log(5 > 2)    // true
+console.log(12 > 12)  // false
+console.log(3 < 0)    // false
+console.log(3 >= 3)   // true
+console.log(11 <= 11) // true
+console.log(3 <= 2)   // false
+
+// let firstCard = 10
+// let secondCard = 11
+// let sum = firstCard + secondCard
+// let hasBlackJack = false
+// let isAlive = true
+
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new card? 🙂")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! 🥳")
+//     hasBlackJack = true
+// } else {
+//     console.log("You're out of the game! 😭")
+//     isAlive = false
+// }
+
+// console.log(isAlive)
+
+ */
