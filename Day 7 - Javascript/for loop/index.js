0a4eb8ad341f88118d447088a0ec0b338a42bb9b
Gemechu Taye <Geme.t07@gmail.com>
2022-02-07 16:26:25 +0530

    for loops and arrays

--- Day 7 - Javascript/for loop/index.js
+++ Day 7 - Javascript/for loop/index.js
@@ -28,3 +28,25 @@ for (let i = 10; i < 101; i += 10) {
 // Use console.log to log out the numbers
 
 */
+
+/* for loops and arrays 
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear",
+    "🙌"
+]
+
+// DRY - Don't repeat yourself
+// console.log(messages[0])
+// console.log(messages[1])
+// console.log(messages[2])
+// console.log(messages[3])
+
+for (let i = 0; i < messages.length; i += 1) {
+    console.log(messages[i])
+}
+
+*/
