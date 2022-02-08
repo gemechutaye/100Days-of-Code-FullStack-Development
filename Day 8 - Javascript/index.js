57b779ce300221187ec9c3e31a8a199a3aabff99
Gemechu Taye <Geme.t07@gmail.com>
2022-02-08 11:47:33 +0530

    I wrote my first logical operator

--- Day 8 - Javascript/index.js
+++ Day 8 - Javascript/index.js
@@ -324,3 +324,50 @@ function newCard() {
 }
 */
 
+/* the logic and operator
+let hasCompletedCourse = true
+let givesCertificate = true
+
+if (hasCompletedCourse === true && givesCertificate === true) {
+    generateCertificate()
+}
+
+function generateCertificate() {
+    console.log("Generating certificate....")
+}
+*/
+
+
+/* my first logical operator
+let hasSolvedChallenge = true
+let hasHintsLeft = false
+
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
+
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+    showSolution()
+}
+
+function showSolution() {
+    console.log("Showing the solution....")
+}
+*/
+
+
+/* the Logic or Operator
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
+
+let likesDocumentaries = true
+let likesStartups = true
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie()
+}
+
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
+}
+
+*/
