e2674ae795e6485be807b438d226fc576bffd59e
Gemechu Taye <Geme.t07@gmail.com>
2022-02-08 11:07:21 +0530

    Generating random numbers

--- Day 8 - Javascript/Blackjack game - Complete/index.js
+++ Day 8 - Javascript/Blackjack game - Complete/index.js
@@ -449,3 +449,244 @@ function newCard() {
 }
 
 */
+
+
+
+/* Generating random numbers with Math.random()
+
+let randomNumber = Math.random()
+
+console.log(randomNumber)
+
+
+// What does Math.random() do?
+
+Your answer: it generates a random number between 0 and 1 (not inclusive of 1)
+
+*/
+
+
+
+/* Math.random() * 6
+let randomNumber = Math.random() * 6
+
+console.log(randomNumber)
+
+// In which range will our randomNumber be now?
+
+From: 0
+To: 5.999999
+*/
+
+
+/* Flooring the number with Math.floor()
+// let randomNumber = Math.random() * 6
+
+let flooredNumber = Math.floor(12.999999999999)
+
+console.log(flooredNumber)
+
+
+// What does Math.floor() do to positive numbers?
+
+Your answer: it removes the decimals
+*/
+
+
+/* 
+Using Math.random() and Math.floor() to create a dice
+
+let randomNumber = Math.floor( Math.random() * 6 )
+
+console.log(randomNumber)
+
+// Write down all the possible values randomNumber can hold now!
+
+ 0,1,2,3,4,5
+
+*/
+
+/* Completing our dice function
+
+// Create a function, rollDice(), that returns a random number between 1 and 6
+
+function rollDice() {
+    let randomNumber = Math.floor( Math.random() * 6 ) + 1
+    return randomNumber
+}
+console.log( rollDice() )
+*/
+
+
+/* Make getRandomCard() work
+
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1
+    return randomNumber
+}
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+
+*/
+
+
+/* Complete getRandomNumber function
+
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function getRandomCard() {
+    // if 1     -> return 11
+    // if 11-13 -> return 10
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+*/
+
+/* Complete getRandomNumber function
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+console.log(cards)
+
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    // Generate two random numbes
+    // Re-assign the cards and sum variables so that the game can start
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+*/
