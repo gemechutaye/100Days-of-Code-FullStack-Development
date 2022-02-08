af9f32615040276617d71becb83d2251d6a1e5d0
Gemechu Taye <Geme.t07@gmail.com>
2022-02-08 12:17:12 +0530

    intro to object

--- Day 8 - Javascript/index.js
+++ Day 8 - Javascript/index.js
@@ -507,3 +507,21 @@ function newCard() {
     }
 }
 */
+
+/* intro to object
+// Objects - store data in-depth - composite / complex data type
+// key-value pairs
+
+let course = {
+    title: "Learn CSS Grid for free",
+    lessons: 16,
+    creator: "Per Harald Borgen",
+    length: 63,
+    level: 2,
+    isFree: true,
+    tags: ["html", "css"]
+}
+
+
+console.log( course.tags )
+*/
