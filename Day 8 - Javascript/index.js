5c835098cec12ecef4320184e26dab857c4b3ecf
Gemechu Taye <Geme.t07@gmail.com>
2022-02-08 12:23:56 +0530

    creat my first object

--- Day 8 - Javascript/index.js
+++ Day 8 - Javascript/index.js
@@ -525,3 +525,20 @@ let course = {
 
 console.log( course.tags )
 */
+
+/* Creat my first obect
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let castle = {
+    title: "Live like a king in my castle",
+    price: 190,
+    isSuperHost: true,
+    images: ["img/castle1.png", "img/caste2.png"]
+}
+
+console.log(castle.price)
+console.log(castle.isSuperHost)
+
+*/
