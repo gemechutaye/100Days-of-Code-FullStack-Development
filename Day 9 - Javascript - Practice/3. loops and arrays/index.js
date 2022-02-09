9378149b60bf9ccdee0c5176bff8fd7f6f5ec6dc
Gemechu Taye <Geme.t07@gmail.com>
2022-02-09 10:07:05 +0530

    loops and arrays

---
+++ Day 9 - Javascript - Practice/3. loops and arrays/index.js
@@ -0,0 +1,15 @@
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+console.log("The 5 largest countries in the world:")
+for (let i = 0; i < largeCountries.length; i++) {
+    console.log("- " + largeCountries[i])
+}
