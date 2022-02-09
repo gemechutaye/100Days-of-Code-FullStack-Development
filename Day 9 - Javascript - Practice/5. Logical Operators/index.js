258e064a6d53ef8a46338dd488bcab0ad64b16d8
Gemechu Taye <Geme.t07@gmail.com>
2022-02-09 10:53:42 +0530

    Logical Operator practice

---
+++ Day 9 - Javascript - Practice/5. Logical Operators/index.js
@@ -0,0 +1,12 @@
+let dayOfMonth = 13
+let weekday = "Tuesday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if (dayOfMonth === 13 && weekday === "Friday") {
+    console.log("😱")
+}
+
+
+
