a574dc516f3351ea75a10203d771cbbb392bf428
Gemechu Taye <Geme.t07@gmail.com>
2022-02-12 11:56:59 +0530

    merged and organized sub-folders

---
+++ Day 12 - JavaScript /Build a chrome extenssion/index.js
@@ -0,0 +1,17 @@
+// Day 12 all practices
+// Creat button and input tags
+/*
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
+*/
+
+// Refactor to AddEventListener
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
+
