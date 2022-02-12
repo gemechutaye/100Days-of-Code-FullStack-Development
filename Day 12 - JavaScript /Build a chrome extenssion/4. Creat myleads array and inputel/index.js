e829ca72beed987dd8a26dfaaf88a5aee1950cd1
Gemechu Taye <Geme.t07@gmail.com>
2022-02-12 12:43:40 +0530

    When to use let and const

--- Day 12 - JavaScript /Build a chrome extenssion/4. Creat myleads array and inputel/index.js
+++ Day 12 - JavaScript /Build a chrome extenssion/4. Creat myleads array and inputel/index.js
@@ -8,5 +8,3 @@ const inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
 })
-
-
--- Day 12 - JavaScript /Build a chrome extenssion/index.js
+++ Day 12 - JavaScript /Build a chrome extenssion/index.js
@@ -41,7 +41,7 @@ inputBtn.addEventListener("click", function() {
 })
 */
 
-
+/*
 // Create two variables:
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
@@ -52,6 +52,28 @@ const inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
 })
+*/
 
 
+
+// When to use let and const
+// If possible, use const. If not, use let.
+
+// Which variables below should be changed from let to const?
+
+// The customer wants to order some stuff. Here are the details:
+const basePrice = 520
+const discount = 120
+let shippingCost = 12
+let shippingTime = "5-12 days"
+
+// Whops! Turns out the shipping will be a bit more complex
+shippingCost = 15
+shippingTime = "7-14 days"
+
+// Calculating the full price
+const fullPrice = basePrice - discount + shippingCost
+
+// Finally, notifying the customer
+console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
 
