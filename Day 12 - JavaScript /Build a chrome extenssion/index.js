c4f4a388fde82abcb99a5d7658d181726e232976
Gemechu Taye <Geme.t07@gmail.com>
2022-02-12 12:16:38 +0530

    refactor from onclick function to addEventListner

--- Day 12 - JavaScript /Build a chrome extenssion/index.js
+++ Day 12 - JavaScript /Build a chrome extenssion/index.js
@@ -22,8 +22,21 @@ inputBtn.addEventListener("click", function() {
 // 2. Add a click event listener to the box 
 // 3. Log out "I want to open the box!" when it's clicked
 
+/*
 let box = document.getElementById("box")
 
 box.addEventListener("click", function() {
     console.log("I want to open the box!")
 })
+*/
+
+// Refactor the code so that it uses .addEventListener()
+// when you click the SAVE INPUT button
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
+
