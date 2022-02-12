35f3892c96cdcdedba6f8f91ab2f2fd8706098e8
Gemechu Taye <Geme.t07@gmail.com>
2022-02-12 12:27:45 +0530

    Create the myLeads array and inputEl

---
+++ Day 12 - JavaScript /Build a chrome extenssion/4. Creat myleads array and inputel/index.js
@@ -0,0 +1,12 @@
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
+
--- Day 12 - JavaScript /Build a chrome extenssion/index.js
+++ Day 12 - JavaScript /Build a chrome extenssion/index.js
@@ -30,6 +30,7 @@ box.addEventListener("click", function() {
 })
 */
 
+/*
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
@@ -38,5 +39,19 @@ let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
 })
+*/
+
+
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
 
 
