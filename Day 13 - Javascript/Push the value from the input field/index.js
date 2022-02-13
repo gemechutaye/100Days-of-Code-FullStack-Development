0aeb7b1fbf99cbc94bb1b87b0475f6f5fd8a7fda
Gemechu Taye <Geme.t07@gmail.com>
2022-02-13 22:31:14 +0530

    Push the value from the input field

--- Day 13 - Javascript/Push the value from the input field/index.js
+++ Day 13 - Javascript/Push the value from the input field/index.js
@@ -1,1 +1,14 @@
 // Push the value from the input field
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+
