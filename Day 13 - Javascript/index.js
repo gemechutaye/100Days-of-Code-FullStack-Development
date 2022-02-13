22dc81a69300d2450e2bc3ef9befcc1afa92db70
Gemechu Taye <Geme.t07@gmail.com>
2022-02-13 22:40:52 +0530

    practice - Use a for loop to log out leads and push new array

---
+++ Day 13 - Javascript/index.js
@@ -0,0 +1,43 @@
+// Today's all javascript practices
+
+// push new array
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push("www.awesomelead.com")
+    console.log(myLeads)
+})
+
+// Push the value from the input field
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+//Use a for loop to log out leads
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+
+// Log out the items in the myLeads array using a for loop 
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
+ 
