dff6e4b70f31e539d929983fd18a856cb46709cd
Gemechu Taye <Geme.t07@gmail.com>
2022-02-14 17:19:00 +0530

    Using CalcEngine app to perform a mathmatical calculations

--- Day 14 - JAVA/Conditional logic/src/Main.java
+++ Day 14 - JAVA/Conditional logic/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello Jain's!");
+	System.out.println("Hello Gorgeous Friends!");
+
+/*    if-else statement
+        int value1 = 10;
+        int value2 = 14;
+
+        if (value1 > value2)
+            System.out.println("Value 1 is greater!");
+        else
+            System.out.println("Wow!, Value 2 is great!");
+*/
+        int value1 = 10;
+        int value2 = 40;
+        if (value1 > value2)
+            System.out.println("Value 1 is Bigger");
+        else if (value1 < value2)
+            System.out.println("value 2 is Bigger");
+        else
+            System.out.println("Value 1 and Value 2 are equal");
+
     }
 }
