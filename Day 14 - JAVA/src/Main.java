5ca4078f1d7a0da01c8cb7496601690c7c61b191
Gemechu Taye <Geme.t07@gmail.com>
2022-02-14 16:00:40 +0530

    A closer look at type conversions

--- Day 14 - JAVA/src/Main.java
+++ Day 14 - JAVA/src/Main.java
@@ -67,6 +67,39 @@ public class Main {
           int val2 = 10;
           myOtherValue /= val1 * val2;
           System.out.println(myOtherValue);
+
+          // A closer look at Operator Precedence
+          int valA = 21;
+          int valB = 6;
+          int valC =3;
+          int valD = 1;
+
+          int result1 = valA - valB / valC;
+
+          int result2 = (valA - valB) / valC;
+
+          System.out.println(result1);
+          System.out.println(result2);
+
+          int result3 = valA / valC * valD + valB;
+
+          int result4 =  valA / (valC * (valD + valB));
+
+          System.out.println(result3);
+          System.out.println(result4);
+
+//          A closer look at type conversions
+          float floatVal = 1.0f;
+          double doubleVal = 4.0d;
+          byte byteVal = 7;
+          short shortVal = 7;
+          long longVal = 5;
+
+          short result1 = (short) longVal;
+          short result2 = (short) (byteVal - longVal);
+          float result3 = longVal - floatVal;
+
+          System.out.println("Success");
       }
 
 }
