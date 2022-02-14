4e6f8e3d3a29f145d65eb2cb691d0a3236d35a6b
Gemechu Taye <Geme.t07@gmail.com>
2022-02-14 14:58:57 +0530

    Data Types

--- Day 14 - JAVA/src/Main.java
+++ Day 14 - JAVA/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
       public static void main(String[] args) {
-          // declaring variables
+//          declaring variables
           int myVar;
           myVar = 50;
           System.out.println(myVar);
@@ -13,9 +13,38 @@ public class Main {
           final int maxStudents = 25;
           System.out.println(maxStudents);
           final int someVariable;
-          int someOtherVariable = 100;
-          someVariable = someOtherVariable;
-          System.out.println(someVariable);
+//          int someOtherVariable = 100;
+//          someVariable = someOtherVariable;
+//          System.out.println(someVariable);
+
+//          Primitive Data Types
+//          Integer Types
+          byte numberOfEnglishLetters = 26;
+          short feetInMile = 5200;
+          int milesToSun = 92960000;
+          long mileInALightYear = 58790000000000L;
+
+//          Floating Point Types
+          float kilometersInAMarathon = 42.195f;
+          float absoluteZeroInCelsius = -273.15f;
+          double atomWidthInMeters = 0.00000000001d;
+
+//          Character Types
+          char regularU = 'U';
+//          char accentedU = '\UooDA'; //U
+
+//          Boolean Type
+          boolean iLoveJava = true;
+
+          // Primitive types stored by value
+          int firstValue = 100;
+          int otherValue = firstValue;
+          System.out.println(firstValue);
+          System.out.println(otherValue);
+          firstValue = 50;
+          System.out.println(firstValue);
+          otherValue = 75;
+          System.out.println(otherValue);
 
       }
 }
