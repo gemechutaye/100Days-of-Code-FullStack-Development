e7f7bf6827d7fdfce0680312b1f719a652b757ac
Gemechu Taye <Geme.t07@gmail.com>
2022-02-14 17:21:31 +0530

    Using CalcEngine app to perform a mathmatical calculations

---
+++ Day 14 - JAVA/CalcEngine/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    // Using CalcEngine app to perform a mathmatical calculations
+
+    public static void main(String[] args) {
+	double value1 = 100.0d;
+    double value2 = 50.0d;
+    double result = 0.0d;
+    char opCode = 'a';
+
+    if(opCode == 'a')
+        result = value1 + value2;
+    else if(opCode == 's')
+        result = value1 - value2;
+    else if(opCode == 'm')
+        result = value1 * value2;
+    else if(opCode == 'd')
+        result = value1 / value2;
+    else
+        result = 0.0d;
+
+    System.out.println(result);
+
+    }
+}
