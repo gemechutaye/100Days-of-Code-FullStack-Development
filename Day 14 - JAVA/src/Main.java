13c47592d93bdf02c4663358def8c299828bbcc0
Gemechu Taye <Geme.t07@gmail.com>
2022-02-14 14:35:54 +0530

    created java file and declaring my first variables

--- Day 14 - JAVA/src/Main.java
+++ Day 14 - JAVA/src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+      public static void main(String[] args) {
+          // declaring variables
+          int myVar;
+          myVar = 50;
+          System.out.println(myVar);
+          int anotherVar = 100;
+          System.out.println(anotherVar);
+          myVar = anotherVar;
+          System.out.println(myVar);
+
+          final int maxStudents = 25;
+          System.out.println(maxStudents);
+          final int someVariable;
+          int someOtherVariable = 100;
+          someVariable = someOtherVariable;
+          System.out.println(someVariable);
+
+      }
 }
+
