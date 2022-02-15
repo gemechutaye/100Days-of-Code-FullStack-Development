6ef3b05ecdd95785c71f80d39ef1f38c073d9fd3
Gemechu Taye <Geme.t07@gmail.com>
2022-02-15 16:58:23 +0530

    Array - CalcEngin - JavaApp

--- Day 15 - JAVA/4. Array - CalcEngin - JavaApp/src/Main.java
+++ Day 15 - JAVA/4. Array - CalcEngin - JavaApp/src/Main.java
@@ -29,4 +29,11 @@ public class Main {
     }
     System.out.println(result);
     }
+
+    // for each loop with arrays
+    float[] theVals = { 10.0f, 20.0f, 15.0f };
+    float sum = 0.0f;
+    for(float currentVal : theVals)
+        sum += currentVal;
+    System.out.println(sum); // dispaly 45
 }
