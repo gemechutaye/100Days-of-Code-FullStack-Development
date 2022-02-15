2e758b4fa18e38cb5e6193f4fb8aafa3b9b78298
Gemechu Taye <Geme.t07@gmail.com>
2022-02-15 17:05:12 +0530

    Array - CalcEngin - JavaApp

--- Day 15 - JAVA/4. Array - CalcEngin - JavaApp/src/Main.java
+++ Day 15 - JAVA/4. Array - CalcEngin - JavaApp/src/Main.java
@@ -5,35 +5,38 @@ public class Main {
     double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
     double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
     char[] opCodes = {'d', 'a', 's', 'm'};
-    double[] result = new double[opCodes.length];
+    double[] results = new double[opCodes.length];
 
     for(int i = 0; i < opCodes.length; i++) {
         switch (opCodes[i]) {
             case 'a':
-                result[i] = leftVals[i] + rightVals[i];
+                results[i] = leftVals[i] + rightVals[i];
                 break;
             case 's':
-                result[i] = leftVals[i] - rightVals[i];
+                results[i] = leftVals[i] - rightVals[i];
                 break;
             case 'm':
-                result[i] = leftVals[i] * rightVals[i];
+                results[i] = leftVals[i] * rightVals[i];
                 break;
             case 'd':
-                result[i] = leftVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
+                results[i] = leftVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
                 break;
             default:
             System.out.println("Invalid opCode: " + opCodes[i]);
-            result[i] = 0.0d;
+            results[i] = 0.0d;
             break;
         }
     }
-    System.out.println(result);
+    for(double currentResult : results)
+        System.out.println(currentResult);
     }
 
     // for each loop with arrays
+    /*
     float[] theVals = { 10.0f, 20.0f, 15.0f };
     float sum = 0.0f;
     for(float currentVal : theVals)
         sum += currentVal;
-    System.out.println(sum); // dispaly 45!
+    System.out.println(sum); // dispaly 45
+     */
 }
