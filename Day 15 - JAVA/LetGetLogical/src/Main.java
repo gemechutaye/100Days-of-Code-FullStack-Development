0cde80472434ad7d34a386189a74af6fd81da84f
Gemechu Taye <Geme.t07@gmail.com>
2022-02-15 10:56:48 +0530

    Logical vs conditional

---
+++ Day 15 - JAVA/LetGetLogical/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Logical operator (&)
+	int a = 20, b = 14, c = 5;
+
+    if ( a > b & b > c)
+        System.out.println("a is greater than c");
+        // Negation
+    boolean done = false;
+    if ( ! done)
+        System.out.println("keep going!");
+
+
+    // Logical vs conditional
+    int students = 150;
+    int rooms = 4;
+
+    if (students/rooms > 30)
+        System.out.println("Crowded!");
+
+
+    System.out.println();
+    System.out.println("*** end of program ***");
+    }
+}
