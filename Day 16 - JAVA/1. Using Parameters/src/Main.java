10381110999f2097a218c29a6d0b1ef43c13b342
Gemechu Taye <Geme.t07@gmail.com>
2022-02-16 10:20:17 +0530

    Using Parameters

---
+++ Day 16 - JAVA/1. Using Parameters/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    /*
+    static void showSum(float x, float y, int count) {
+        float sum = x + y; // sum 8.9
+        for(int i = 0, i < count; i++);
+        System.out.println(sum);
+    }
+     */
+}
