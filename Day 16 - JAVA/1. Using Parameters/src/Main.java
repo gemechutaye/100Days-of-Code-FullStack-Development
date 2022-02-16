0dc9e4c807705698a67e0770dcfc47312e220a9a
Gemechu Taye <Geme.t07@gmail.com>
2022-02-16 10:31:55 +0530

    Method changes to parameter values

--- Day 16 - JAVA/1. Using Parameters/src/Main.java
+++ Day 16 - JAVA/1. Using Parameters/src/Main.java
@@ -6,4 +6,16 @@ public class Main {
         System.out.println(sum);
     }
      */
+//    Method changes to parameter values
+    static void swap(int i, int j) {
+        int k = i;
+        i = j;
+        j = k;
+    // i:20 j:10
+        int val1 = 10;
+        int val2 = 20;
+        swap(val1, val2);
+        System.out.println(val1); // 10
+        System.out.println(val2); // 20
+    }
 }
