ff129f49ca240e3bcbbe5e76ec2ccc2c8a606f86
Gemechu Taye <Geme.t07@gmail.com>
2022-02-16 10:31:55 +0530

    Method changes to parameter values

--- Day 16 - JAVA/1. Using Parameters/src/Main.java
+++ Day 16 - JAVA/1. Using Parameters/src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main<Static> {
     /*
     static void showSum(float x, float y, int count) {
         float sum = x + y; // sum 8.9
@@ -6,4 +6,30 @@ public class Main {
         System.out.println(sum);
     }
      */
+//    Method changes to parameter values
+    /*
+    static void swap(int i, int j) {
+        int k = i;
+        i = j;
+        j = k;
+    // i:20 j:10
+        int val1 = 10;
+        int val2 = 20;
+        swap(val1, val2);
+        System.out.println(val1); // 10
+        System.out.println(val2); // 20
+    }
+     */
+//   Exiting a Method
+    Static void showSum(float x, float y, int count) {
+    showSum(7.5, 1.4, 0);
+    System.out.printin("Back from showSum");
+
+        if (count < 1)
+            return;
+        float sum = x + y;
+        for(int i = 0, i < count; i++);
+            System.out.println(sum);
+        return;
+    }
 }
