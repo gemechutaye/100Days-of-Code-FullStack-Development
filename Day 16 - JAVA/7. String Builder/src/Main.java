fa1a94d48f367d9f801665207d623eff329e9ed4
Gemechu Taye <Geme.t07@gmail.com>
2022-02-16 17:29:17 +0530

    String Builder

--- Day 16 - JAVA/7. String Builder/src/Main.java
+++ Day 16 - JAVA/7. String Builder/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String location = "Florida";
+    int flightNumber = 175;
+    StringBuilder sb = new StringBuilder(40);
+    sb.append("I flew to ");
+    sb.append(location);
+    sb.append(" on Flight #");
+    sb.append(flightNumber);
+    String message = sb.toString();
+    System.out.println(message);
+        String time = "9:00";
+        int pos = sb.indexOf(" on");
+        sb.insert(pos, " at ");
+        sb.insert(pos + 4, time);
+        message = sb.toString();
+        System.out.println(message);
     }
 }
