78a744f90b1810f2bca222d9189539830ece7e8d
Gemechu Taye <Geme.t07@gmail.com>
2022-02-16 11:38:01 +0530

    CalcEngin Command-line Arguments

---
+++ Day 16 - JAVA/3. CalcEngin - Command-line Arguments/src/main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// Array - CalcEngin - JavaApp
+    double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+    double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+    char[] opCodes = {'d', 'a', 's', 'm'};
+    double[] results = new double[opCodes.length];
+
+    if (args.length == 0 ) {
+        for (int i = 0; i < opCodes.length; i++) {
+            results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+        }
+
+        for (double currentResult : results)
+            System.out.println(currentResult);
+    } else if(args.length == 3)
+        handleCommandLine(args);
+    else
+        System.out.println("Please provide an operation code and 2 numeric values");
+
+    }
+
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal) {
+        double result;
+
+        switch (opCode) {
+            case 'a' -> result = leftVal + rightVal;
+            case 's' -> result = leftVal - rightVal;
+            case 'm' -> result = leftVal * rightVal;
+            case 'd' -> result = leftVal != 0 ? leftVal / rightVal : 0.0d;
+            default -> {
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+            }
+        }
+
+        return result;
+    }
+}
