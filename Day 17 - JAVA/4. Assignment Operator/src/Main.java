009d1dff1ae47e6fd27eeaf7ef97936977a33d98
Gemechu Taye <Geme.t07@gmail.com>
2022-02-17 11:09:20 +0530

    Assignment Operator

---
+++ Day 17 - JAVA/4. Assignment Operator/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 4;
+    int var;
+
+    // assign value using =
+        var = a;
+        System.out.println("var using =: " + var);
+
+        // Assign value using =+
+        var += a;
+        System.out.println("var using +=: " + var);
+
+        // assign value using =-
+        var -= a;
+        System.out.println("var using -=: " + var);
+
+        // assign value using =/
+        var /= a;
+        System.out.println("var using /+: " + var);
+
+        // assign value using =*
+        var *= a;
+        System.out.println("var using *=: " + var);
+
+    }
+}
