0e00392a1425094ed7e413a1bee3bdfa74a11067
Gemechu Taye <Geme.t07@gmail.com>
2022-03-09 22:35:56 +0530

    JavaScript Get Properties Values of an Object

---
+++ Day 37 & 38 - Javascript/3. Get properties values of an object/index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>JavaScript Get Properties Values of an Object</title>
+</head>
+<body>
+    <script>
+    var book = {
+        "name": "Harry Potter and the Goblet of Fire",
+        "author": "J. K. Rowling",
+        "year": 2000
+    };
+    
+    // Dot notation
+    document.write(book.name + "<br>");  // Prints: Harry Potter and the Goblet of Fire
+    document.write(book.author + "<br>");  // Prints: J. K. Rowling
+    document.write(book.year);  // Prints: 2000
+    document.write("<hr>");
+    
+    // Bracket notation
+    document.write(book["name"] + "<br>");  // Prints: Harry Potter and the Goblet of Fire
+    document.write(book["author"] + "<br>");  // Prints: J. K. Rowling
+    document.write(book["year"]);  // Prints: 2000
+    </script>
+</body>
+</html>
