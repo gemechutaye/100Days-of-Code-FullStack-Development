05f56705a341f98675cba9640719c316ae8607a0
Gemechu Taye <gemetayeh@gmail.com>
2022-03-17 21:57:49 +0530

    Calculates the sum of the two input arrays.

---
+++ Day 45 - 46 (JS)/1. Lucky Numbers/index.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+ export function twoSum(array1, array2) {
+    throw new Error('Implement the twoSum function');
+  }
+  
+  /**
+   * Checks whether a number is a palindrome.
+   *
+   * @param {number} value
+   * @returns {boolean}  whether the number is a palindrome or not
+   */
+  export function luckyNumber(value) {
+    throw new Error('Implement the luckyNumber function');
+  }
+  
+  /**
+   * Determines the error message that should be shown to the user
+   * for the given input value.
+   *
+   * @param {string|null|undefined} input
+   * @returns {string} error message
+   */
+  export function errorMessage(input) {
+    throw new Error('Implement the errorMessage function');
+  }
+
+  //  inspired by: https://github.com/exercism/javascript/blob/main/exercises/concept/lucky-numbers/lucky-numbers.js
