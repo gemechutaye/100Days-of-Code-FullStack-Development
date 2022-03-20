d9008f8e4eee1facdcb14daef7538a51d90ed7ed
Gemechu Taye <Geme.t07@gmail.com>
2022-03-20 17:50:23 +0530

    Refactor to addEventListener

--- Day 48 - Build Chrome Extensions/index.js
+++ Day 48 - Build Chrome Extensions/index.js
@@ -1,5 +1,16 @@
+/*
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
 
 function saveLead() {
     console.log("Button clicked!")
 }
+
+*/
+
+// Refactor to addEventListener
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
