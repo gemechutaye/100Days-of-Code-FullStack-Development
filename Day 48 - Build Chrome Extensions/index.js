08c005762538cb551268510537f0432fcd82dad1
Gemechu Taye <Geme.t07@gmail.com>
2022-03-20 17:58:32 +0530

    I wrote my\ first addEventListener()

--- Day 48 - Build Chrome Extensions/index.js
+++ Day 48 - Build Chrome Extensions/index.js
@@ -8,9 +8,25 @@ function saveLead() {
 */
 
 // Refactor to addEventListener
+/*
 
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked from addEventListener")
 })
+
+*/
+
+// I Wrote your first addEventListener() 
+
+// 1. Grab the box from the DOM and store it in a variable
+// 2. Add a click event listener to the box 
+// 3. Log out "I want to open the box!" when it's clicked
+
+let box = document.getElementById("box")
+
+box.addEventListener("click", function() {
+    console.log("I want to open the box!")
+})
+
