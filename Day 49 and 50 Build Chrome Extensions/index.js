11c0d5acb06b6d4905f5f55bc06d18f68bb9b344
Gemechu Taye <Geme.t07@gmail.com>
2022-03-22 00:08:14 +0530

    I wrote my second addEventListener()

---
+++ Day 49 and 50 Build Chrome Extensions/index.js
@@ -0,0 +1,46 @@
+/*
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+function saveLead() {
+    console.log("Button clicked!")
+}
+*/
+
+
+// Refactor to addEventListener
+/*
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
+*/
+
+
+/*
+// I Wrote my first addEventListener() 
+
+// 1. Grab the box from the DOM and store it in a variable
+// 2. Add a click event listener to the box 
+// 3. Log out "I want to open the box!" when it's clicked
+
+let box = document.getElementById("box")
+
+box.addEventListener("click", function() {
+    console.log("I want to open the box!")
+})
+*/
+
+
+// Refactor the code so that it uses .addEventListener()
+// when you click the SAVE INPUT button
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
+
+
+
