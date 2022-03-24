bd8b5ca6233fa8e4c2536d81ab28c0be2e0cf737
Gemechu Taye <Geme.t07@gmail.com>
2022-03-24 11:04:33 +0530

    More innerHTML practice

--- Day 51 and 52 Build Chrome Extensions/2. Buy Button/index.js
+++ Day 51 and 52 Build Chrome Extensions/2. Buy Button/index.js
@@ -1,6 +1,20 @@
-// Use .innerHTML to render a Buy! button inside the div container
-
+/* Use .innerHTML to render a Buy! button inside the div container
 
 const container = document.getElementById("container")
-
 container.innerHTML = "<button>Buy!</button>"
+*/
+
+
+
+/* More innerHTML practice
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// When clicked, render a paragraph under the button (in the container)
+// that says "Thank you for buying!"
+
+function buy(){
+    container.innerHTML = "<p>Thank you for buying!</p>"
+}
+*/
