84ad82b0f718cccc6d70f2becf1740cc431cf764
Gemechu Taye <Geme.t07@gmail.com>
2022-03-25 23:13:40 +0530

    Improving the performance of our app

--- Day 53 Build Chrome Extensions/index.js
+++ Day 53 Build Chrome Extensions/index.js
@@ -16,7 +16,7 @@ for (let i = 0; i < myLeads.length; i++) {
 */
 
 
-// Use createElement() and append() instead of innerHTML
+/* Use createElement() and append() instead of innerHTML
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -37,6 +37,32 @@ for (let i = 0; i < myLeads.length; i++) {
     li.textContent = myLeads[i]
     ulEl.append(li)
 }
+*/
+
+// Improving the performance of our app
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+    listItems += "<li>" + myLeads[i] + "</li>"
+}
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems
+
+
+
+
 
 
 
