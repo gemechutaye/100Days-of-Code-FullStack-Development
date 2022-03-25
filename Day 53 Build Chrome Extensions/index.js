ce7442fbb4849c0ac9183d6eec9517248f6904f7
Gemechu Taye <Geme.t07@gmail.com>
2022-03-25 23:00:24 +0530

    Render the <li> elements with innerHTML

--- Day 53 Build Chrome Extensions/index.js
+++ Day 53 Build Chrome Extensions/index.js
@@ -1,121 +1,18 @@
-/*
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
-
-function saveLead() {
-    console.log("Button clicked!")
-}
-*/
-
-
-// Refactor to addEventListener
-/*
-
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
-})
-*/
-
-
-/*
-// I Wrote my first addEventListener() 
-
-// 1. Grab the box from the DOM and store it in a variable
-// 2. Add a click event listener to the box 
-// 3. Log out "I want to open the box!" when it's clicked
-
-let box = document.getElementById("box")
-
-box.addEventListener("click", function() {
-    console.log("I want to open the box!")
-})
-*/
-
-/*
-// Refactor the code so that it uses .addEventListener()
-// when you click the SAVE INPUT button
-
-let inputBtn = document.getElementById("input-btn")
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
-})
-*/
-
-
-/*
-// When to use let and const
-
-// If possible, use const. If not, use let.
-
-// Which variables below should be changed from let to const?
-
-// The customer wants to order some stuff. Here are the details:
-const basePrice = 520
-const discount = 120
-let shippingCost = 12
-let shippingTime = "5-12 days"
-
-// Whops! Turns out the shipping will be a bit more complex
-shippingCost = 15
-shippingTime = "7-14 days"
-
-// Calculating the full price
-const fullPrice = basePrice - discount + shippingCost
-
-// Finally, notifying the customer
-console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
-*/
-
-
-/*
-// Pushe the myLeads array[]
-let myLeads = []
+// Render the <li> elements with innerHTML
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push("www.awesomelead.com")
-    console.log(myLeads)
-})
-*/
-
-/*
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click", function() {
-    // Push the value from the inputEl into the myLeads array 
-    // instead of the hard-coded "www.awesomeleads.com" value
-    // Google -> "get value from input field javascript"
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-})
-<<<<<<< HEAD
-*/
-=======
-*/
-
-
-// Use for loop to logout Leads
-/*
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com" "www.greatlead.com"]
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
-
-// Log out the items in the myLeads array using a for loop 
+// Replace .textContent with .innerHTML and use <li> tags
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
-*/
 
-// >>>>>>> 1cb3a30 (Push the value from the input fields)
+
+
