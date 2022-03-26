e0a9e64538036172938c6514154b25f1fac1bd5f
Gemechu Taye <Geme.t07@gmail.com>
2022-03-26 17:22:36 +0530

    Using template Strings

--- Day 54 Build Chrome Extensions/index.js
+++ Day 54 Build Chrome Extensions/index.js
@@ -1,4 +1,4 @@
-// Add anchor tag (<a>) inside the <li>
+/* Add anchor tag (<a>) inside the <li>
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -17,6 +17,35 @@ function renderLeads() {
         // Can you make the link open in a new tab?
         listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         console.log(listItems)
+    }
+    ulEl.innerHTML = listItems  
+}
+*/
+
+
+// Template Strings
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+            <li>
+                <a target="_blank" href="${myLeads[i]}">
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems  
 }
