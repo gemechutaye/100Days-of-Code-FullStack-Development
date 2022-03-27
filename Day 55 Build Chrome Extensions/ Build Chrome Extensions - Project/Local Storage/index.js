a3dc978aae5251b7b96222345e9ce1e303dfb1ce
Gemechu Taye <Geme.t07@gmail.com>
2022-03-27 14:56:42 +0530

    Save the leads to local storage

--- Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Local Storage/index.js
+++ Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Local Storage/index.js
@@ -83,3 +83,35 @@ function renderLeads() {
     ulEl.innerHTML = listItems  
 }
 */
+
+// Save the leads to local storage
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
