c413727ac9872514b09eae78c06c7db0611d9564
Gemechu Taye <Geme.t07@gmail.com>
2022-03-27 14:52:44 +0530

    Storing arrays in local storage

--- Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Local Storage/index.js
+++ Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Local Storage/index.js
@@ -1,3 +1,4 @@
+/* My first Local storage
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -9,8 +10,6 @@ let name = localStorage.getItem("myName")
 console.log(name)
 
 // localStorage.clear()
-
-
 
 // 1. Save a key-value pair in localStorage
 // 2. Refresh the page. Get the value and log it to the console
@@ -42,3 +41,45 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems  
 }
+*/
+
+
+
+/* Storing arrays in local storage
+
+let myLeads = `["www.awesomelead.com"]`
+
+// 1. Turn the myLeads string into an array
+myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array
+myLeads.push("www.Geboss.org")
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads) 
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
+
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+*/
