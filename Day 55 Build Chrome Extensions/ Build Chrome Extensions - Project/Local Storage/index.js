ec22ca155bf89effd3b479d25838c3eb2829e00c
Gemechu Taye <Geme.t07@gmail.com>
2022-03-27 15:01:36 +0530

    Get the leads from local Storage

--- Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Local Storage/index.js
+++ Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Local Storage/index.js
@@ -84,7 +84,7 @@ function renderLeads() {
 }
 */
 
-// Save the leads to local storage
+/* Save the leads to local storage
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -115,3 +115,43 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems  
 }
+*/
+
+/* Get the leads from local Storage
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+console.log(leadsFromLocalStorage)
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+*/
