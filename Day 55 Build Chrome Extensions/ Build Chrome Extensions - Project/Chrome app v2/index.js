4c8ec68d052642624eca7477654f8770d41d0987
Gemechu Taye <Geme.t07@gmail.com>
2022-03-27 16:12:15 +0530

    How function parameters can improve our code

--- Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Chrome app v2/index.js
+++ Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Chrome app v2/index.js
@@ -38,7 +38,7 @@ function renderLeads() {
 */
 
 
-// Make the delete button works
+/* Make the delete button works
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -81,3 +81,44 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems  
 }
+*/
+
+let myLeads = []
+let oldLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
