8e666b44715c050bfb3ccb99ca559c8255f966d0
Gemechu Taye <Geme.t07@gmail.com>
2022-03-27 14:35:51 +0530

    My first local storage

---
+++ Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Local Storage/index.js
@@ -0,0 +1,44 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+// localStorage.setItem("myName", "Per Harald Borgen")
+
+let name = localStorage.getItem("myName")
+console.log(name)
+
+// localStorage.clear()
+
+
+
+// 1. Save a key-value pair in localStorage
+// 2. Refresh the page. Get the value and log it to the console
+// 3. Clear localStorage
+
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
