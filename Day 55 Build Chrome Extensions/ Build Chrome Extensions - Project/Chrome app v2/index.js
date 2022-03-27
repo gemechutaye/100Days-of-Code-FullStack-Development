fc0fc57506605e7724653cc83b165da8d802e9ef
Gemechu Taye <Geme.t07@gmail.com>
2022-03-27 16:38:22 +0530

    numbers as function parameteres

--- Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Chrome app v2/index.js
+++ Day 55 Build Chrome Extensions/ Build Chrome Extensions - Project/Chrome app v2/index.js
@@ -128,10 +128,22 @@ inputBtn.addEventListener("click", function() {
 // Functions with multiple parameteres
 const welcomeEl = document.getElementById("welcome-el")
 
-// Add the ability to choose the emoji as well!
+/* Add the ability to choose the emoji as well!
 
 function greetUser(greeting, name, emoji) {
     welcomeEl.textContent = `${greeting}, ${name} ${emoji}`
 }
 
 greetUser("Howdy", "James", "🔥")
+*/
+
+/*  numbers as function parameteres
+// Create a function, add(), that adds two numbers together and returns the sum
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+console.log( add(3, 4)   ) // should log 7
+console.log( add(9, 102) ) // should log 111
+*/
