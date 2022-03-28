25fb4f0a1cefe86cb0944aa4a6b48467e2324be0
Gemechu Taye <Geme.t07@gmail.com>
2022-03-28 15:36:22 +0530

    Save to local storage

--- Day 56 Javascript recap/index.js
+++ Day 56 Javascript recap/index.js
@@ -34,3 +34,12 @@ function logItems(arr) {
 
 logItems(myCourses)
 */
+
+/* Save to local storage
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
+*/
