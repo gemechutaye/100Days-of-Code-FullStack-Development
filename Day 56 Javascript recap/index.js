56db43da531892d45085b5d5aa39d34837ef6233
Gemechu Taye <Geme.t07@gmail.com>
2022-03-28 16:36:18 +0530

    Render images

--- Day 56 Javascript recap/index.js
+++ Day 56 Javascript recap/index.js
@@ -43,3 +43,26 @@ logItems(myCourses)
 let myCredits = localStorage.getItem("myCredits")
 console.log(myCredits)
 */
+
+// Render images
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
+
+const imgs = [
+    "images/hip1.jpg",
+    "images/hip2.jpg",
+    "images/hip3.jpg"
+]
+
+const container = document.getElementById("container")
+
+function renderImages() {
+    let imgsDOM = ""
+    for (let i = 0; i < imgs.length; i++) {
+        imgsDOM += `<img alt="Employee in the company" class="team-img" src="${imgs[i]}">`
+    }
+    container.innerHTML = imgsDOM
+}
+
+renderImages()
