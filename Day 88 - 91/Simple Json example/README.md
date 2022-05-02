0e1aa991a7ba4afdee76e58a2dcc545f700d53d7
Gemechu Taye <Geme.t07@gmail.com>
2022-05-02 23:52:52 +0530

    Simple Json example

---
+++ Day 88 - 91/Simple Json example/README.md
@@ -0,0 +1,5 @@
+# Simple Json example
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rogerpence/pen/AXwdwN](https://codepen.io/rogerpence/pen/AXwdwN).
+
+This Pen shows a very simple use of using Json with JavaScript. 
---
+++ Day 88 - 91/Simple Json example/dist/script.js
@@ -0,0 +1,41 @@
+console.clear()
+
+var drive = {
+        "CPU": "Intel",
+        "Cores": 8,
+        "Drives": [
+            "DVD read/writer",
+            "1TB Sata 7200",
+            "250GB SSD",
+            32
+        ],
+        "Manufacturer": {
+            "Name": "Bob's Drives",
+            "Street": "331 Jefferson",
+            "City": "Gas City",
+            "State": "IN",
+            "ZipCode": 78216
+        }
+    };
+
+console.log(drive.Drives[0]);
+console.log(drive.Drives[3]);
+console.log(drive.Manufacturer.Name);
+
+// An alternative way to create a JavaScript object:
+
+var manufacturer = new Object();
+manufacturer.Name = "Bob's Drives";
+manufacturer.Street = "331 Jefferson";
+manufacturer.City = "Gas City";
+manufacturer.State = "IN";
+manufacturer.ZipCode = 78216;
+
+var drive2 = new Object();
+drive2.CPU = "Intel";
+drive2.Cores = 8;
+drive2.Drives = ["DVD read/writer", "1TB Sata 7200", "250GB SSD"];
+drive2.Manufacturer = manufacturer; 
+
+console.log(drive2.Drives[0]);
+console.log(drive2.Manufacturer.Name);
